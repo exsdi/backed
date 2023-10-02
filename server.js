bebc4f699a70d36192c6b49900f47a8512312165
exsdi <sandiegoexclusive@gmail.com>
2023-10-02 16:23:05 -0700

    adding cors to backend

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts.js');
 
 // express app
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
