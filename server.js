8f7eefb67c49a121808426054c65e84af6b11bae
exsdi <sandiegoexclusive@gmail.com>
2023-10-02 00:32:38 -0700

    cors origin error

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const cors = require('cors');
 // express app
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     console.log(req.path, req.method);
     next();
 })
